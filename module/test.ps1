e7c33f1509469820ed46d1a60cf652dffb593f6f
Christoph Burmeister <christoph.burmeister@outlook.de>
2024-08-21 19:18:00 +0200

    feat: added even more output

--- module/test.ps1
+++ module/test.ps1
@@ -3,5 +3,8 @@ Write-Output "Hello World!"
 Write-Output "another one!"
 Write-Output "another one!"
 Write-Output "another one!"
+Write-Output "another one!"
+Write-Output "another one!"
+Write-Output "another one!"
 # I'm a comment!
 Get-Host
